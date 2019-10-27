fee61205c607fb1de946607b4c88cf6ffe3e7015
Phil Thompson <phil@electricvisions.com>
2019-10-27 12:12:59 +0100

    Build script traverses content dir and generates html from source code

--- source/build.fsx
+++ source/build.fsx
@@ -1,17 +1,54 @@
 #r "paket:
 source https://api.nuget.org/v3/index.json
+source https://ci.appveyor.com/nuget/fsharp-formatting
 
 nuget Fake.IO.FileSystem
 nuget Fake.Core.Trace
 nuget FSharp.Data
-nuget FSharp.Formatting //"
-
-#load "packages/fsharp.formatting/3.1.0/FSharp.Formatting.fsx"
-
-open FSharp.Formatting.Razor
+nuget Fable.React
+nuget FSharp.Literate //"
 
 let source = "../content"
 let output = "../output"
 let template = "template.html"
 
-RazorLiterate.ProcessDirectory (source, template, output)
+open System.IO
+open FSharp.Literate
+
+let rec processFiles pattern dirs =
+  if Seq.isEmpty dirs then Seq.empty else
+    seq {
+      yield! dirs |> Seq.collect (fun dir -> Directory.EnumerateFiles(dir, pattern))
+      yield! dirs |> Seq.collect Directory.EnumerateDirectories |> processFiles pattern
+    }
+
+let parse source =
+  let doc =
+    let fsharpCoreDir = "-I:" + __SOURCE_DIRECTORY__ + "/../lib"
+    let systemRuntime = "-r:System.Runtime"
+    Literate.ParseScriptString(
+      source,
+      compilerOptions = systemRuntime + " " + fsharpCoreDir,
+      fsiEvaluator = FSharp.Literate.FsiEvaluator([|fsharpCoreDir|])
+    )
+
+  FSharp.Literate.Literate.FormatLiterateNodes(doc, OutputKind.Html, "", true, true)
+
+let format (doc: LiterateDocument) =
+  Formatting.format doc.MarkdownDocument true OutputKind.Html
+
+let toOutputPath (path: string) =
+  path.Replace(source, output).Replace(".fsx", ".html")
+
+let processFile path =
+  let outputPath = toOutputPath path
+
+  File.WriteAllText (outputPath,
+    path
+    |> File.ReadAllText
+    |> parse
+    |> format
+  )
+
+processFiles "*.fsx" [source]
+|> Seq.iter processFile
