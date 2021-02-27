10b8f1a5c09b3bbd884be2b2002857fcec815ac1
Phil Thompson <phil@electricvisions.com>
2021-02-27 20:03:23 +0000

    Fix typo in class name, add some instruction on cache invalidation

--- release.ps1
+++ release.ps1
@@ -1,4 +1,9 @@
 echo 'Uploading docs to S3...'
 aws s3 sync output/electricvisions s3://electricvisions.com --delete
 aws s3 sync output/matter-game s3://matter-game.com --delete
+
 echo 'Done.'
+echo 'To invalidate CloudFront cached objects go to:'
+echo 'https://console.aws.amazon.com/cloudfront'
+echo 'Select site, click Invalidations, Create Invalidation'
+echo 'Enter "/*" and click Invalidate'
